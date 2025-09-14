71c10a706b772086d23fce43d6224e665c2212a1
gray <thegraycolour@mail.ru>
2025-09-14 17:37:00 +0300

    feat: Changed website visuals and added courses

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,10 +27,6 @@ const Contact = () => {
 
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
           <ImageClipBox
-            src={content?.contact?.images?.swordmanPartial || "/img/swordman-partial.webp"}
-            clipClass="absolute md:scale-125"
-          />
-          <ImageClipBox
             src={content?.contact?.images?.swordman || "/img/swordman.webp"}
             clipClass="sword-man-clip-path md:scale-125"
           />
