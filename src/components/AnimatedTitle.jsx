c85fba4155e170f594df9fb3f0d3c3ba9dc03142
yeabwang <yeabsiratesfaye781@gmail.com>
2025-09-14 23:12:03 +0800

    feat: Update home page title for clarity and consistency; refactor AnimatedTitle component by removing GSAP animations

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,41 +1,8 @@
-import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import clsx from "clsx";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const AnimatedTitle = ({ title, containerClass }) => {
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      const titleAnimation = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "100 bottom",
-          end: "center bottom",
-          toggleActions: "play none none reverse",
-        },
-      });
-
-      titleAnimation.to(
-        ".animated-word",
-        {
-          opacity: 1,
-          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
-          ease: "power2.inOut",
-          stagger: 0.02,
-        },
-        0
-      );
-    }, containerRef);
-
-    return () => ctx.revert(); // Clean up on unmount
-  }, []);
-
   return (
-    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
+    <div className={clsx("animated-title", containerClass)}>
       {title.split("<br />").map((line, index) => (
         <div
           key={index}
@@ -45,6 +12,7 @@ const AnimatedTitle = ({ title, containerClass }) => {
             <span
               key={idx}
               className="animated-word"
+              style={{ opacity: 1, transform: "none" }}
               dangerouslySetInnerHTML={{ __html: word }}
             />
           ))}
